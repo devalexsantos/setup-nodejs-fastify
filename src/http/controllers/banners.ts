a7175ca311db3d3d9fbe8fd7fade32e28b5a4a3f
Alex Santos <devalexsantos@gmail.com>
2023-11-11 04:36:15 -0300

    increment a crud with delete, create and update

--- src/http/controllers/banners.ts
+++ src/http/controllers/banners.ts
@@ -6,4 +6,78 @@ export async function getBanners() {
     const banners = await prisma.banners.findMany()
   
     return banners
-  }
+}
+
+export async function getBanner(request: FastifyRequest, reply: FastifyReply) {
+  const paramSchema = z.object({
+    id: z.string()
+  })
+
+  const { id } = paramSchema.parse(request.params)
+
+  const banner = await prisma.banners.findMany({
+    where: {
+      id
+    }
+  })
+
+  return banner
+}
+
+export async function createBanner(request: FastifyRequest, reply: FastifyReply){
+  const bodySchema = z.object({
+    description: z.string(),
+    url: z.string(),
+  })
+
+  const {description, url} = bodySchema.parse(request.body)
+  
+  await prisma.banners.create({
+    data: {
+      description,
+      url
+    }
+  })
+
+  return reply.status(201).send()
+}
+
+export async function updateBanner(request: FastifyRequest, reply: FastifyReply){
+  const bodySchema = z.object({
+    description: z.string().optional(),
+    url: z.string()
+  })
+
+  const paramSchema = z.object({ id: z.string() })
+
+  const {description, url} = bodySchema.parse(request.body)
+  const { id } = paramSchema.parse(request.params)
+
+  await prisma.banners.update({
+    where: {
+      id: id
+    },
+    data: {
+      description,
+      url
+    }
+  })
+
+  return reply.status(204).send('Banner Updated!')
+}
+
+export async function deleteBanner(request: FastifyRequest, reply: FastifyReply) {
+  const paramSchema = z.object({
+    id: z.string()
+  })
+
+  const { id } = paramSchema.parse(request.params)
+
+  await prisma.banners.delete({
+    where: {
+      id
+    }
+  })
+
+  return reply.status(204).send()
+}
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,12 @@
 import { FastifyInstance } from 'fastify'
-import { getBanners } from './controllers/banners'
+import { createBanner, deleteBanner, getBanner, getBanners, updateBanner } from './controllers/banners'
 
 export async function appRoutes(app: FastifyInstance) {
     app.get('/banners', getBanners)
+    app.get('/banners/:id', getBanner)
+
+    app.post('/banners', createBanner)
+    app.post('/banners/:id', updateBanner)
+
+    app.delete('/banners/delete/:id', deleteBanner)
   }
